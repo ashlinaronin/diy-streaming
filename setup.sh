a6cfa87c866bd71283455f61203ee5ade5eddce8
Ashlin Aronin <ashlinaronin@gmail.com>
2025-09-01 16:48:55 -0700

    update urls in setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@
 source .env
 
 # reserve public shares for your servers so they can have stable URLs
-zrok reserve public --unique-name "$STREAMING_SERVER_NAME" http://localhost:4533
-zrok reserve public --unique-name "$FILE_SERVER_NAME" http://localhost:8080
+zrok reserve public --unique-name "$STREAMING_SERVER_NAME" http://navidrome:4533
+zrok reserve public --unique-name "$FILE_SERVER_NAME" http://nginx:8080
